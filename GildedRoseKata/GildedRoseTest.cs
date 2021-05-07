fc99af02babc183df7c1d265a7c9f972f47a0201
Nadhir Messaoud <nadhir.messaoud@cellenza.com>
2021-05-07 15:33:14 +0200

    Ajoutez des fichiers projet.

---
+++ GildedRoseKata/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("Aged Brie", Items[0].Name);
+        }
+    }
+}
